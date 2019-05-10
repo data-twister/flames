c1f5650cae03c9e80955a86b12327a6112e417c1
Jason Clark <mithereal@gmail.com>
2019-05-10 13:11:57 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -53,12 +53,13 @@ defmodule Flames.Mixfile do
 
   defp deps do
     [
-      {:ecto, "~> 3.0"},
-      {:phoenix, "~> 1.4.1", optional: true},
+
+      {:ecto, ">= 2.0"},
+      {:phoenix, ">= 1.1", optional: true},
       {:ex_doc, ">= 0.0.0", only: [:docs, :dev]},
       {:earmark, ">= 0.0.0", only: [:docs, :dev]},
-      {:phoenix_ecto, "~> 4.0", only: :test},
-      {:phoenix_html, "~> 2.9", only: :test},
+      {:phoenix_ecto, ">= 2.0", only: :test},
+      {:phoenix_html, ">= 2.3", only: :test},
     ]
   end
 
