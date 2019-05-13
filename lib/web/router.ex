80ebecc04d3778965e0228dcb103e7f38b7cb321
Jason Clark <mithereal@gmail.com>
2019-05-13 11:47:41 -0700

    .

--- lib/web/router.ex
+++ lib/web/router.ex
@@ -10,6 +10,9 @@ if Code.ensure_loaded?(Phoenix.Router) do
 
     pipeline :browser do
       plug(:accepts, ~w(html))
+      plug (:fetch_session)
+      plug (:protect_from_forgery)
+    plug (:put_secure_browser_headers)
       plug(:fetch_flash)
     end
 
