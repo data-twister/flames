03c5e7f14d1ac21863b3aa73ab4f86f1930e5dd2
Jason Clark <mithereal@gmail.com>
2019-05-13 11:42:11 -0700

    .

--- lib/web/web.ex
+++ lib/web/web.ex
@@ -4,7 +4,11 @@ defmodule Flames.Web do
   def view do
     quote do
       use Phoenix.View, root: "lib/web/templates"
+      # Import convenience functions from controllers
+      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+
       use Phoenix.HTML
+
       import Phoenix.LiveView, only: [live_render: 2, live_render: 3]
     end
   end
@@ -24,6 +28,9 @@ defmodule Flames.Web do
 
   def router do
     quote do
+      use Phoenix.Router
+      import Plug.Conn
+      import Phoenix.Controller
       import Phoenix.LiveView.Router
     end
   end
